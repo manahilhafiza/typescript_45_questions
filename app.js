72e727641e99ab9c7cbad139f1e3e7367c749ffc
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:36:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var megicians_names = ['harry poter', 'hermione', 'albus', 'ron'];
+function make_great(megicianarry) {
+    for (var i = 0; i < megicianarry.length; i++)
+        megicians_names[i] = +'the great' + megicianarry[i];
+}
 function show_megicians(megician) {
     megician.forEach(function (element) {
         console.log(element);
     });
 }
 show_megicians(megicians_names);
+make_great(megicians_names);
--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 let megicians_names: string [] = ['harry poter','hermione','albus','ron'];
 
+
+function make_great (megicianarry: string []){
+    for(let i = 0; i < megicianarry.length ; i++)
+    megicians_names [i] =   'the great' + megicianarry [i];
+
+}
+
 function show_megicians (megician:string[]){
    megician.forEach(element => {
       console.log(element);
    });
 }
  show_megicians(megicians_names)
-
+  make_great(megicians_names)
