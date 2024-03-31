a6e8e9b462cf6fed4c834589ee6c29e601bd6642
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:36:48 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,16 +1,30 @@
-let megicians_names: string [] = ['harry poter','hermione','albus','ron'];
+let megicians_names: string [] = ['harry poter ','hermione ','albus ','ron'];
 
+function copy_Array (arr:string[]){
+    return[...arr]
+}
 
 function make_great (megicianarry: string []){
     for(let i = 0; i < megicianarry.length ; i++)
-    megicians_names [i] =   'the great' + megicianarry [i];
+
+    megicianarry [i] =   ' the Great ' + megicianarry [i];
 
 }
 
-function show_megicians (megician:string[]){
+function show_megicians ( megician:string[]){
+
    megician.forEach(element => {
+
       console.log(element);
    });
 }
- show_megicians(megicians_names)
-  make_great(megicians_names)
+ const megicianArraycopy = copy_Array(megicians_names)
+
+  make_great(megicianArraycopy);
+
+  console.log('\n\nthis is my orignal array');
+
+  show_megicians(megicians_names);
+  console.log('\n\nthis is my modify copy of the  array')
+
+ show_megicians(megicianArraycopy);
