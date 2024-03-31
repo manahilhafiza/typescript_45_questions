1c6b19b7b3e0bda06c478e029f8d8f70fdb097e2
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:22:34 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-// Exercise 24
-// Exercise 25
+// version 1 of the program
 var alien_color = ("green");
 if (alien_color === "green") {
-    console.log("player just earn 5 points for shooting the alien");
+    console.log("  player just earn 5 points ");
+}
+else if (alien_color === "yellow") {
+    console.log("player just earne 10 points");
+}
+else if (alien_color === "red") {
+    console.log("version 1: player just earn 15 points!");
 }
 else {
-    console.log(" player just earn 10 points!");
+    console.log(" please selrct right color!");
 }
-if (alien_color === "yellow") {
-    console.log("player earne 10 points for shooting the alien");
+// version 2 of the program
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player just earne 10 points ");
+}
+else if (alien_color === "red") {
+    console.log(" version 2: player just earn 15 points!");
 }
 else {
-    console.log(" player just earn 10 points!");
+    console.log(" please selrct right color!");
+}
+// version 3 of the proram
+alien_color = "red";
+if (alien_color === "green") {
+    console.log("player just earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player just earn 10 points ");
+}
+else if (alien_color === "red") {
+    console.log(" player just earn 15 ");
+}
+else {
+    console.log("please select right colors ");
 }
--- app.ts
+++ app.ts
@@ -1,19 +1,45 @@
-// Exercise 24
-// Exercise 25
+// version 1 of the program
+
 let alien_color: string = ("green");
 
 if(alien_color === "green"){
-    console.log("player just earn 5 points for shooting the alien");
-}else{
-    console.log(" player just earn 10 points!");
-
+    console.log("  player just earn 5 points ");
 }
- if (alien_color === "yellow"){
-      console.log("player earne 10 points for shooting the alien");
+else  if (alien_color === "yellow"){
+      console.log("player just earne 10 points");
+}
+else if ( alien_color === "red"){
+    console.log("version 1: player just earn 15 points!");
 }else{
-    console.log(" player just earn 10 points!");
+    console.log(" please selrct right color!");
 }
 
+// version 2 of the program
 
+ alien_color = "yellow";
 
+ if(alien_color === "green"){
+ console.log("player just earn 5 points");
+ }
+ else  if (alien_color === "yellow"){
+    console.log("player just earne 10 points ");
+ }
+ else if ( alien_color === "red"){
+console.log(" version 2: player just earn 15 points!");
+}else{
+    console.log(" please selrct right color!");
+}
+// version 3 of the proram
+alien_color = "red";
 
+if(alien_color === "green"){
+console.log("player just earn 5 points");
+}
+else  if (alien_color === "yellow"){
+console.log("player just earn 10 points ");
+}
+else  if (alien_color === "red"){
+console.log(" player just earn 15 ");
+} else{
+    console.log("please select right colors ");
+}  
