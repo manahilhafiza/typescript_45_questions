d469cda15f2edc340f129b0cd8bf44927249aac8
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:16:53 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-//guest list: 3 GUEST INVITED
-var Guest_List = ['Kamran Tessori', 'Nawaz Sharef', 'Asif Ali'];
-//for(let i = 0; i<Guest_List.length; i++){
-// console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
-//}
-//absent guest
-var absent_Guest = 'Kamran Tessori';
-//new guest
-var new_Guest = 'Daniyal';
-Guest_List[0] = new_Guest;
-//for(let i = 0; i<Guest_List.length; i++){
-// console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
-//}
-//console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
-//3 more guest invited:
-//console.log('Good News! we find big dinner table so we are inviting 3 more guest.');
-Guest_List.unshift('Sir Zia');
-Guest_List.splice(2, 0, 'Maryum Nawaz');
-Guest_List.push('Bilawal Bhutto');
-//for(let i = 0; i<Guest_List.length; i++){
-//console.log('Dear Mr.' + Guest_List [i] + ',\n\n It Is Our Pleasure to invite You in  Our Party. \n\n Thank You!\n\n');
-//}   
-// REMOVE GUEST LIST ONLY 2 GUEST INVITED:
-//console.log('\N Sorry we can not arrange big table, only 2 peoples will be invited.');
-while (Guest_List.length > 2) {
-    var Remove_Guest = Guest_List.pop();
-    //console.log (`Dear Mr. sorry! ${Remove_Guest} you can not invited to dinner.`);
+var items = ['Pakistan', 'Karachi', 'Urdu'];
+console.log(items);
+for (var i = 0; i < items.length; i++) {
+    console.log("".concat(items[i]));
 }
-//STILL INVITED GUEST:
-//for(let i = 0; i<Guest_List.length; i++){
-//console.log('Dear Mr.' + Guest_List [i] + ',\n\n you are still invited to dinner. \n\n Thank You! \n\n');
-//}
-//EMPTY GUEST LIST
-Guest_List.splice(0, 2);
-console.log(Guest_List);
-//Exercise 19 
-console.log("total number of guest + ".concat(Guest_List.length));
--- app.ts
+++ app.ts
@@ -1,53 +1,6 @@
-//guest list: 3 GUEST INVITED
-let Guest_List: string  [] = ['Kamran Tessori','Nawaz Sharef','Asif Ali'];
-
-//for(let i = 0; i<Guest_List.length; i++){
-
-   // console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
-//}
-//absent guest
-
-let absent_Guest: string = 'Kamran Tessori';
-
-//new guest
-
-let new_Guest: string = 'Daniyal';
-
-Guest_List[0] = new_Guest;
-
-//for(let i = 0; i<Guest_List.length; i++){
-   // console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
-//}
-//console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
-
-//3 more guest invited:
-//console.log('Good News! we find big dinner table so we are inviting 3 more guest.');
-
-Guest_List.unshift('Sir Zia');
-
-Guest_List.splice( 2, 0 ,'Maryum Nawaz');
-
-Guest_List.push('Bilawal Bhutto');
-
-//for(let i = 0; i<Guest_List.length; i++){
-//console.log('Dear Mr.' + Guest_List [i] + ',\n\n It Is Our Pleasure to invite You in  Our Party. \n\n Thank You!\n\n');
-//}   
-  
-// REMOVE GUEST LIST ONLY 2 GUEST INVITED:
-//console.log('\N Sorry we can not arrange big table, only 2 peoples will be invited.');
-
-while(Guest_List.length>2){
-let Remove_Guest = Guest_List.pop(); 
-//console.log (`Dear Mr. sorry! ${Remove_Guest} you can not invited to dinner.`);
+let items: string [] = ['Pakistan','Karachi','Urdu'];
+console.log(items);
+for(let i=0; i<items.length; i++){
+    console.log(`${items[i]}`);
 }
-//STILL INVITED GUEST:
-//for(let i = 0; i<Guest_List.length; i++){
-//console.log('Dear Mr.' + Guest_List [i] + ',\n\n you are still invited to dinner. \n\n Thank You! \n\n');
- //}
-//EMPTY GUEST LIST
-Guest_List.splice(0,2);
-console.log(Guest_List);
-
-//Exercise 19 
-console.log(`total number of guest + ${Guest_List.length}`);
 
