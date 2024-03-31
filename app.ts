b83007efa4e56b70043b4a892ab983889ff66c06
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:04:37 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,5 +1,38 @@
-let favorite_number: number = 3;
-let message: string = 'Here is my favorite number ' + favorite_number ;
-console.log(message);
+//program 1
+//program written by: Manahil
+ // Current date . 9 .march 
+ // this program will  write hello world
+
+ //console.log("hello world");
+
+
+//program 2
+
+
+//program written by: Manahil
+// Current date . 9 .march 
+// this program will  write addition / subtraction / multiplication / division:
+
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
 
 
