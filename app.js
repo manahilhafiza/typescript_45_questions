38b87f5149510aa64b98c9d7ecd72de59900e25d
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:12:47 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var Guest_List = ['Kamran Tessori', 'Nawaz Sharef', 'Asif Ali'];
 for (var i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr.' + Guest_List[i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
 }
+var absent_Guest = 'Kamran Tessori';
+var new_Guest = 'Daniyal';
+Guest_List[0] = new_Guest;
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
+}
+console.log("Dear Mr. ".concat(absent_Guest, " is not coming to the party."));
--- app.ts
+++ app.ts
@@ -1,5 +1,21 @@
 let Guest_List: string  [] = ['Kamran Tessori','Nawaz Sharef','Asif Ali'];
+
 for(let i = 0; i<Guest_List.length; i++){
 
-    console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+    console.log('Dear Mr.' + Guest_List [i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
 }
+
+
+let absent_Guest: string = 'Kamran Tessori';
+
+let new_Guest: string = 'Daniyal';
+
+Guest_List[0] = new_Guest;
+
+for(let i = 0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr.' + Guest_List [i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
+  
+}   
+
+console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
