90aa5587dee5a1189275d6823dc8683cd62d8939
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:29:56 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-    var num = numbers_1[_i];
-    var ordinalEnding = void 0;
-    if (num === 1) {
-        ordinalEnding = "st";
-    }
-    else if (num === 2) {
-        ordinalEnding = "nd";
-    }
-    else if (num === 3) {
-        ordinalEnding = "rd";
-    }
-    else {
-        ordinalEnding = "th";
-    }
-    console.log("".concat(num, " ").concat(ordinalEnding));
+var pizzas = ['B B Q', 'Mecroni', 'Pepperoni'];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
 }
+console.log("i really like pizza");
--- app.ts
+++ app.ts
@@ -1,21 +1,7 @@
-let numbers: number  [] =  [1,2,3,4,5,6,7,8,9];
+let pizzas: string [] = ['B B Q','Mecroni','Pepperoni'];
 
-for(let num of numbers ){
+for(let pizza of pizzas){
 
-  let ordinalEnding: string;
-
-  if(num === 1){
-       ordinalEnding = "st";
-  }
- else if(num === 2){
-        ordinalEnding = "nd";
- }
- else if (num === 3){
-      ordinalEnding = "rd";
- }
- else {
-      ordinalEnding = "th";
- }
- console.log(`${num} ${ordinalEnding}`);
-
+    console.log(`I like ${pizza} pizza.`);
 }
+console.log(`i really like pizza`);
