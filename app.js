d0423d504c93b3f2c41741760a0a54dccbea1caf
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:06:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var Names = ['sadia', 'javeria', 'nida', 'minal'];
-for (var i = 0; i < Names.length; i++) {
-    console.log(Names[i]);
+var Guest_List = ['Kamran Tessori', 'Nawaz Sharef', 'Asif Ali'];
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
 }
--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-let Names: string [] = ['sadia','javeria','nida','minal'];
-for(let i = 0; i < Names.length; i ++){                       
-console.log(Names[i]);
- }
+let Guest_List: string  [] = ['Kamran Tessori','Nawaz Sharef','Asif Ali'];
+for(let i = 0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+}
