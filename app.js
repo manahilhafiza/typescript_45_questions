0a8bd70f70d545ea9bffe2405311dbe7c0817892
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-31 07:13:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+//guest list:
 var Guest_List = ['Kamran Tessori', 'Nawaz Sharef', 'Asif Ali'];
-for (var i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr.' + Guest_List[i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
-}
+//for(let i = 0; i<Guest_List.length; i++){
+// console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+//}
+//absent guest
 var absent_Guest = 'Kamran Tessori';
+//new guest
 var new_Guest = 'Daniyal';
 Guest_List[0] = new_Guest;
 for (var i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr.' + Guest_List[i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
 }
 console.log("Dear Mr. ".concat(absent_Guest, " is not coming to the party."));
+//3 more guest invited:
+console.log('Good News! we find big dinner table so we are inviting 3 more guest.');
+Guest_List.unshift('Sir Zia');
+Guest_List.splice(2, 0, 'Maryum Nawaz');
+Guest_List.push('Bilawal Bhutto');
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\n It Is Our Pleasure to invite You in  Our Party. \n\n Thank You!');
+}
--- app.ts
+++ app.ts
@@ -1,21 +1,35 @@
+//guest list:
 let Guest_List: string  [] = ['Kamran Tessori','Nawaz Sharef','Asif Ali'];
 
-for(let i = 0; i<Guest_List.length; i++){
+//for(let i = 0; i<Guest_List.length; i++){
 
-    console.log('Dear Mr.' + Guest_List [i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
-}
-
+   // console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+//}
+//absent guest
 
 let absent_Guest: string = 'Kamran Tessori';
+
+//new guest
 
 let new_Guest: string = 'Daniyal';
 
 Guest_List[0] = new_Guest;
 
 for(let i = 0; i<Guest_List.length; i++){
-
-    console.log('Dear Mr.' + Guest_List [i] + ',\n\n It is Our Pleasure to invite you in our Party. \n\n Thank You!');
-  
-}   
-
+    console.log('Dear Mr.' + Guest_List [i] + ',\n\n IT IS OUR PLEASURE TO INVITE YOU IN OUR PARTY. \n\n Thank You!');
+}
 console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
+
+//3 more guest invited:
+console.log('Good News! we find big dinner table so we are inviting 3 more guest.');
+
+Guest_List.unshift('Sir Zia');
+
+Guest_List.splice( 2, 0 ,'Maryum Nawaz');
+
+Guest_List.push('Bilawal Bhutto');
+
+for(let i = 0; i<Guest_List.length; i++){
+console.log('Dear Mr.' + Guest_List [i] + ',\n\n It Is Our Pleasure to invite You in  Our Party. \n\n Thank You!');
+}   
+  
